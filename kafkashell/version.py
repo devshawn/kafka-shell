36158950ad3251956cfa9ace8451feee7f16338d
Shawn Seymour <shawn@devshawn.com>
2020-02-10 21:42:39 -0600

    release: 0.1.4!

--- kafkashell/version.py
+++ kafkashell/version.py
@@ -13,7 +13,7 @@
 #  ANY KIND, either express or implied. See the License for the specific
 #  language governing permissions and limitations under the License.
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 
 def get_version():
